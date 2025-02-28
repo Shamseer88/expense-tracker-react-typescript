de5df31d79549727556f17a33f1bdfd964a2f957
shamseer8877 <alishamseer@gmail.com>
2025-02-28 20:01:50 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-500 text-xl font-bold">Tailwind is working!</div>
+    </>
+  );
+}
+
+export default App;
